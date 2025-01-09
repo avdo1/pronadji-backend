5419f7e9e91f109d3d212ebc26bd16584b36b71a
avdo01 <abdullah.sp195@gmail.com>
2025-01-09 11:17:07 +0100

    fixed database config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,13 +4,13 @@ config({ path: `${process.env.NODE_ENV || "development"}.env` });
 
 export const databaseConfig: DataSourceOptions = {
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_DATABASE || "pronadji",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   port: parseInt(process.env.DB_PORT || "5432", 10),
   entities: ["dist/**/*.entity{.ts,.js}"],
-  migrations: ["dist/migrations/*.js"],
+  migrations: [process.env.DB_MIGRATIONS],
   synchronize: false,
   logging: true,
 };
